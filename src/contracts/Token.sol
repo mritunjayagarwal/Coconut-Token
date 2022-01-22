64d65a249165ac21c40cb7d584ace50a3652c046
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2022-01-22 13:07:38 +0530

    first commit

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,5 +1,12 @@
 pragma solidity ^0.5.16;
 
 contract Token {
-    string public name = "My Name";
+    string public name = "Coconut Token";
+    string public symbol = "COCO";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    constructor() public{
+        totalSupply = 1000000 * (10 ** decimals);
+    }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -4,15 +4,36 @@ require('chai') //used for testing purposes
 .use(require('chai-as-promised'))
 .should()
 contract('Token', (accounts) => {
+    const name = 'Coconut Token';
+    const symbol = 'COCO';
+    const decimals = '18';
+    const totalSupply = '1000000000000000000000000';
+    let token;
+
+    beforeEach(async() => { //only mocha allows this function
+        token = await Token.new(); //Fetch token from blockchain, same as const token = await Token.deployed();
+    })
+
     describe('deployment', () => {
         it('tracks the name', async () => {
-            const  token = await Token.new(); //same as const token = await Token.deployed();
-            const result = await token.name();
-            result.should.equal('My Name');
-            //Fetch token from blockchain
-            //Read token name here....
-            //The token name is 'My Name'
+            
+            const result = await token.name(); //Read token name here....
+            result.should.equal(name); //The token name is 'My Name'
+        })
 
+        it('tracks the symbol', async() => {
+            const result = await token.symbol();
+            result.should.equal(symbol);
+        })
+
+        it('tracks the decimals', async() => {
+            const result = await token.decimals(); 
+            result.toString().should.equal(decimals);
+        })
+
+        it('tracks the total supply', async() => {
+            const result = await token.totalSupply();
+            result.toString().should.equal(totalSupply);
         })
     })
 })
